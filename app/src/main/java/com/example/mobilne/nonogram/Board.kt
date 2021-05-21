996a96d5ba7a15e30b657bb34f6a30c896ae7392
Casered <casered@protonmail.com>
2021-05-21 03:22:20 +0200

    Pakiet zawierający logikę i elementy niezbędne do skonstruowania gry w nonogramy.

---
+++ app/src/main/java/com/example/mobilne/nonogram/Board.kt
@@ -0,0 +1,33 @@
+package com.example.mobilne.nonogram
+
+class Board /* : GridLayout */ {
+
+    private Tile[][] tiles /*= new Tile[][];*/
+    private Boolean[][] answerKey
+    private String[] tempKey = [
+        "...xxxx...",
+        "....xx....",
+        "....xx....",
+        "x...xx,..x",
+        "xxxxxxxxxx",
+        "xxxxxxxxxx",
+        "x...xx...x",
+        "....xx....",
+        "....xx....",
+        "...xxxx..."
+    ];
+
+
+    //public fun initBoard() {
+    public fun Board(int x, int y /*, File sourcefile */) {
+
+        tiles = new Tile[x][y]
+
+        //answerKey = loadFile(sourcefile)
+        answerKey = loadFile(tempKey)
+
+        /* WORK IN PROGRESS */
+        /*     (spanko)     */
+
+    }
+}
