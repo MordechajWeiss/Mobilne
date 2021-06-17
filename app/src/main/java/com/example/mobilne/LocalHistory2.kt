065244255136fd9e99c9ffa4afd5794fe6cb9342
GmbH2088 <62901844+GmbH2088@users.noreply.github.com>
2021-06-17 12:51:11 +0200

    LocalHistory 2 ready

--- app/src/main/java/com/example/mobilne/LocalHistory2.kt
+++ app/src/main/java/com/example/mobilne/LocalHistory2.kt
@@ -1,8 +1,13 @@
 package com.example.mobilne
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.room.Room
 import kotlinx.coroutines.GlobalScope
@@ -38,12 +43,30 @@ class LocalHistory2 : AppCompatActivity() {
         }
         Thread.sleep(1000)
 
-        var textView = findViewById<TextView>(R.id.LH2_TV_TMP)
+        val layout = findViewById<View>(R.id.l2) as LinearLayout
 
-        if(list.isNotEmpty()){
-                textView.text = textView.text.toString() + "\nMystery number: " + list.get(0).move.toString()
-            for(i in 1..list.size-1){
-                textView.text = textView.text.toString() + "\nMove: " + list.get(i).move.toString()
+        if(list.isNotEmpty())
+        {
+            var param = LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT);
+            param.bottomMargin=18;
+            var newTV = TextView(this);
+            newTV.layoutParams = param;
+            newTV.text = "Mystery number: " + list.get(0).move.toString();
+            newTV.setTextColor(Color.WHITE);
+            newTV.textSize= 36F;
+            layout.addView(newTV)
+
+            for((ct, i) in (1 until list.size).withIndex())
+            {
+                var param = LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT);
+                param.bottomMargin=10;
+
+                newTV = TextView(this);
+                newTV.layoutParams = param;
+                newTV.text = "Guess no."+ ct +" : " + list.get(i).move.toString();
+                newTV.setTextColor(Color.WHITE);
+                newTV.textSize= 32F;
+                layout.addView(newTV);
             }
         }
 
